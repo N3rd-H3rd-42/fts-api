739da85ce2793649835ae22fb9f5887553eaf308
Josh McLain <joshmclain45@gmail.com>
2022-06-06 14:58:30 -0700

    missed

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-const patientController = require('./patient.controller');
+const patientController = require("./patient.controller");
 
 router
   .route("/patient")
   .post(patientController.createOne)
-
-router
-  .route("/patient/:patientId")
   .put(patientController.updateOne);
 
-router.route('/patients').get(patientController.getAll);
+router.route("/patients").get(patientController.getAll);
 
-router.route("/patient/toggle-active").put(patientController.toggleActive);
+router
+  .route("/patient/toggle-active/:patientId")
+  .put(patientController.toggleActive);
 
 module.exports = router;
