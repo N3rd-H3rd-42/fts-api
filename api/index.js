5167923aa8ebb1338ebcddf1e29988511ea0c564
Josh McLain <joshmclain45@gmail.com>
2022-06-06 15:29:48 -0700

    started email service endpoint

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const patientController = require("./patient.controller");
+const emailController = require('./email.controller');
 
 router
   .route("/patient")
@@ -11,5 +12,7 @@ router.route("/patients").get(patientController.getAll);
 router
   .route("/patient/toggle-active/:patientId")
   .put(patientController.toggleActive);
+
+router.route('/send-email').post(emailController.sendEmailNotification);
 
 module.exports = router;
---
+++ constants/mailOptions.js
@@ -0,0 +1,6 @@
+module.exports = {
+  from: "joshmclain@no-reply.com",
+  to: ["jrmclain85@gmail.com"],
+  subject: "Nodemailer Project",
+  text: "Hi from your nodemailer project",
+};
