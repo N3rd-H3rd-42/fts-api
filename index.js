ca951f15f5ee9792340d147c76c29b3b85a4c70e
Josh McLain <joshmclain45@gmail.com>
2022-06-06 01:41:16 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGO_URI, {}, () => console.log('db conntected'));
+
+app.listen(PORT, () => console.log(`app running on port ${PORT}`))
+
+app.route('/').get((req, res) => res.json({}))
