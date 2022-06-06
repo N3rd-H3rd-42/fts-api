36553ef536d9222ca4cf8eb4e91f27684a8f465d
Josh McLain <joshmclain45@gmail.com>
2022-06-06 02:00:13 -0700

    patient controller

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 const router = require("express").Router();
+const patientController = require('./patient.controller');
+
+router
+  .route("/patient")
+  .post(patientController.createOne)
+  .put(patientController.updateOne);
+
+router.route("/patient/toggle-active").put(patientController.toggleActive);
 
 module.exports = router;
